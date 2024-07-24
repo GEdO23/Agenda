04877c35eac09b9e811f3a7ccb54ee039bbed501
Gabriel Eringer <rm99632@fiap.com.br>
2024-07-24 17:00:20 -0300

    AbstractDAO

--- app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
+++ app/src/main/java/br/com/alura/agenda/dao/AlunoDAO.java
@@ -1,59 +1,54 @@
 package br.com.alura.agenda.dao;
 
-import android.util.Log;
+import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import br.com.alura.agenda.model.Aluno;
 
-public class AlunoDAO {
+public class AlunoDAO implements AbstractDAO<Aluno> {
 
-    private final static List<Aluno> alunos = new ArrayList<Aluno>();
+    private final static List<Aluno> alunos = new ArrayList<>();
     private static int contadorDeIds = 1;
 
+    @Override
     public List<Aluno> getAll() {
-        Log.i("lista_alunos", "getAll chamado:" + alunos);
         return new ArrayList<>(alunos);
     }
 
-    public void save(Aluno aluno) {
-        aluno.setId(contadorDeIds);
-        alunos.add(aluno);
-        Log.i("new_aluno", "Novo aluno adcionado:" + aluno.getNome());
-        atualizaIds();
-    }
-
-    private static void atualizaIds() {
-        contadorDeIds++;
-    }
-
-    public void edit(Aluno aluno) {
-        Aluno alunoEncontrado = buscaAlunoPeloId(aluno);
-        if (alunoEncontrado != null) {
-            int posicaoDoAluno = alunos.indexOf(alunoEncontrado);
-            alunos.set(posicaoDoAluno, aluno);
-            Log.i("edit_aluno", "Aluno editado:" + aluno.getNome());
-        }
-    }
-
-    private static Aluno buscaAlunoPeloId(Aluno aluno) {
+    @Override
+    public Aluno getById(@NonNull Aluno aluno) {
         for (Aluno a :
                 alunos) {
             if (a.getId() == aluno.getId()) {
-                Log.i("aluno", "Aluno encontrado:" + aluno.getNome());
                 return a;
             }
         }
         return null;
     }
 
+    @Override
+    public void save(@NonNull Aluno aluno) {
+        aluno.setId(contadorDeIds);
+        alunos.add(aluno);
+        contadorDeIds++;
+    }
 
-    public void remove(Aluno aluno) {
-        Aluno alunoDevolvido = buscaAlunoPeloId(aluno);
+    @Override
+    public void edit(@NonNull Aluno aluno) {
+        Aluno alunoEncontrado = getById(aluno);
+        if (alunoEncontrado != null) {
+            int posicaoDoAluno = alunos.indexOf(alunoEncontrado);
+            alunos.set(posicaoDoAluno, aluno);
+        }
+    }
+
+    @Override
+    public void remove(@NonNull Aluno aluno) {
+        Aluno alunoDevolvido = getById(aluno);
         if (alunoDevolvido != null) {
             alunos.remove(alunoDevolvido);
-            Log.i("remove_aluno", "Aluno removido:" + aluno.getNome());
         }
     }
 }
